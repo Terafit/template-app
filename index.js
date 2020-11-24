68edd5fe60ab91533ae882b56a77499867bf2899
BuildTools <unconfigured@null.spigotmc.org>
2020-11-24 14:59:03 +0500

    ferst app

---
+++ index.js
@@ -0,0 +1,29 @@
+//Подрубаем электрон с этими параметрами
+const { app, BrowserWindow } = require('electron');
+
+//Функция создания окна
+function createWindow() {
+    //Объект параметров окна
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        //По моему это на счёт того можно ли юзать node в проэкте
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    //Подгружаем файл
+    win.loadFile('index.html')
+    //win.webContents.openDevTools()
+}
+
+//Запуск пронграммы функцией app.whenReady() и передаём then(createWindow) - наши параметры окна
+app.whenReady().then(createWindow)
+
+//Эта штука для того чтобы приложение при закрытии выключалось, а не сворачивалось(По моему это только на Mac) Не основная штука в приложении
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
